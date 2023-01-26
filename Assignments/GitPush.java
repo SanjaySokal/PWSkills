0edfb2dd388550ca48e10c273f754daf009581c8
SanjaySokal <sokalsanjay@gmail.com>
2023-01-26 07:26:52 +0530

    First Commit try

---
+++ Assignments/GitPush.java
@@ -0,0 +1,8 @@
+package Assignments;
+
+public class GitPush {
+    public static void main(String[] args) {
+        System.out.println("Hello Git I am testing it");
+        System.out.println("I have made some changes in the file");
+    }
+}
