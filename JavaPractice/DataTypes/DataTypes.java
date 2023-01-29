8ae2f4d6f81c9fa9f5bb57cb27b12e44e90daeee
SanjaySokal <sokalsanjay@gmail.com>
2023-01-29 06:20:16 +0530

    creating Practice folder

---
+++ JavaPractice/DataTypes/DataTypes.java
@@ -0,0 +1,7 @@
+package JavaPractice.DataTypes;
+
+public class DataTypes {
+    public static void main(String[] args) {
+        System.out.println("DataTypes");
+    }
+}
---
+++ JavaPractice/Variables/Variables.java
@@ -0,0 +1,7 @@
+package JavaPractice.Variables;
+
+public class Variables {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
