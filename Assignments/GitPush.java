e553010bac6f64513c1e5de5f282762c334bd44e
SanjaySokal <sokalsanjay@gmail.com>
2023-01-30 10:38:08 +0530

    25 Jan Assignment

--- Assignments/GitPush.java
+++ Assignments/GitPush.java
@@ -4,5 +4,9 @@ public class GitPush {
     public static void main(String[] args) {
         System.out.println("Hello Git I am testing it");
         System.out.println("I have made some changes in the file");
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.print(i + " ");
+        }
     }
 }
