ff88fc80f1cd09549a6a6d40120df4b04af1f9ae
SanjaySokal <sokalsanjay@gmail.com>
2023-02-12 16:14:34 +0530

    10 Feb 2023 Assignment

---
+++ JavaPractice/Construction/Testing.java
@@ -0,0 +1,11 @@
+package JavaPractice.Construction;
+
+public class Testing {
+    Testing() {
+        System.out.println("This is an default constructor");
+    }
+
+    public static void main(String[] args) {
+        Testing testing = new Testing();
+    }
+}
