0533614d7db61965d4657c03c94f7e0cb4c5755d
SanjaySokal <sokalsanjay@gmail.com>
2023-02-12 13:37:53 +0530

    07-08 Feb 2023 Assignment

---
+++ JavaPractice/Static/FirseProgramm.java
@@ -0,0 +1,11 @@
+package JavaPractice.Static;
+
+public class FirseProgramm {
+    public static void main(String[] args) {
+        displayHello();
+    }
+
+    public static void displayHello() {
+        System.out.println("Printing in displayHello() function");
+    }
+}
