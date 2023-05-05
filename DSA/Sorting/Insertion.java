6c4dc2db7d432c56700c540dee6b31f1c850d259
SanjaySokal <sokalsanjay@gmail.com>
2023-05-05 07:03:59 +0530

    inserion sort

--- DSA/Sorting/Insertion.java
+++ DSA/Sorting/Insertion.java
@@ -1,21 +1,23 @@
 package DSA.Sorting;
 
-public class InsertionDescending {
+public class Insertion {
     public static void main(String[] args) {
-        int[] arr = { 3, 5, 1, 6, 0 };
+        int[] arr = { 5, 2, 1, 6, 8, 4, 7, 9, 3, 0 };
 
         int temp, j;
-
         for (int i = 1; i < arr.length; i++) {
             temp = arr[i];
             j = i - 1;
-            // temp = 3 && j = 3
+            // temp = 6
+            // j = 2
 
-            while (j >= 0 && arr[j] < temp) {
+            while (j >= 0 && arr[j] > temp) {
                 arr[j + 1] = arr[j];
                 j--;
             }
+
             arr[j + 1] = temp;
+            // j + 1 = 3
         }
 
         for (int i = 0; i < arr.length; i++) {
