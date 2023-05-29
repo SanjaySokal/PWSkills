2ec6d19ef851f5ce7830cf4f1a1fb5216e109d15
SanjaySokal <sokalsanjay@gmail.com>
2023-05-29 07:30:11 +0530

    after many days

--- DSA/Sorting/BubbleDescending.java
+++ DSA/Sorting/BubbleDescending.java
@@ -3,7 +3,7 @@ package DSA.Sorting;
 public class BubbleDescending {
     public static void main(String[] args) {
         int[] arr = { 3, 5, 1, 6, 0 };
-        for (int i = 0; i < arr.length; i++) {
+        for (int i = 0; i < arr.length - 1; i++) {
             for (int j = 0; j < arr.length - 1 - i; j++) {
                 int temp = 0;
                 if (arr[j] < arr[j + 1]) {
--- DSA/Sorting/SelectionDescending.java
+++ DSA/Sorting/SelectionDescending.java
@@ -3,15 +3,16 @@ package DSA.Sorting;
 public class SelectionDescending {
     public static void main(String[] args) {
         int[] arr = { 3, 5, 1, 6, 0 };
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr.length - 1 - i; j++) {
-                int temp = 0;
-                if (arr[j] < arr[j + 1]) {
-                    temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
+        for (int i = 0; i < arr.length - 1; i++) {
+            int smallest = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[smallest] > arr[j]) {
+                    smallest = j;
                 }
             }
+            int temp = arr[smallest];
+            arr[smallest] = arr[i];
+            arr[i] = temp;
         }
 
         for (int i = 0; i < arr.length; i++) {
